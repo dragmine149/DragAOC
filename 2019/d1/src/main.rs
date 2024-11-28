95349f820aeb69a15f1639262276b7253bd8ca86
dragmine <dan@thebanners.uk>
2024-11-28 14:29:47 +0000

    2019/1/2 completed

--- 2019/d1/src/main.rs
+++ 2019/d1/src/main.rs
@@ -4,11 +4,11 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-fn caluclate_fuel_for_mass(mass: f32) -> i32 {
+fn caluclate_fuel_for_mass(mass: f32) -> f32 {
     let div3: f32 = mass / 3.0;
     let round: f32 = div3.floor();
     let sub2: f32 = round - 2.0;
-    sub2 as i32
+    sub2
 }
 
 fn main() {
@@ -20,7 +20,7 @@ fn main() {
     let file = File::open(file_path).expect("File should be able to be read");
     let reader = BufReader::new(file);
 
-    let mut total_fuel = 0;
+    let mut total_fuel: f32 = 0.0;
     for line in reader.lines() {
         let mass = line.expect("Failed to read line");
 
@@ -28,11 +28,32 @@ fn main() {
             continue;
         }
 
-        let fuel_for_mass = caluclate_fuel_for_mass(mass.parse::<f32>().unwrap());
-        total_fuel += fuel_for_mass;
+        if part == "1" {
+            let fuel_for_mass = caluclate_fuel_for_mass(mass.parse::<f32>().unwrap());
+            total_fuel += fuel_for_mass;
+            if debug == "1" {
+                println!("The fuel required for a mass of '{mass}' is '{fuel_for_mass}'");
+            }
+        }
 
-        if debug == "1" {
-            println!("The fuel required for a mass of '{mass}' is '{fuel_for_mass}'");
+        if part == "2" {
+            if debug == "1" {
+                println!("Calculating fuel for mass: {mass}");
+            }
+
+            let mut loop_mass: f32 = mass.parse::<f32>().unwrap();
+            'fuel_loop: loop {
+                loop_mass = caluclate_fuel_for_mass(loop_mass);
+                if debug == "1" {
+                    println!("Fuel: {loop_mass}");
+                }
+
+                if loop_mass <= 0.0 {
+                    break 'fuel_loop;
+                }
+
+                total_fuel += loop_mass;
+            }
         }
     }
 
