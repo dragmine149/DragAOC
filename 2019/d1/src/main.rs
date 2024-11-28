f8c9e4f56546eb9c8b1cb2bae0ffa00d2a61aff7
dragmine <dan@thebanners.uk>
2024-11-28 14:31:51 +0000

    Comments

--- 2019/d1/src/main.rs
+++ 2019/d1/src/main.rs
@@ -4,6 +4,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+// Runs the calculation to get the fuel for the specified mass.
 fn caluclate_fuel_for_mass(mass: f32) -> f32 {
     let div3: f32 = mass / 3.0;
     let round: f32 = div3.floor();
@@ -12,18 +13,22 @@ fn caluclate_fuel_for_mass(mass: f32) -> f32 {
 }
 
 fn main() {
+    // get cli inputs
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let part = &args[2];
     let debug = &args[3];
 
+    // gets data
     let file = File::open(file_path).expect("File should be able to be read");
     let reader = BufReader::new(file);
 
+    // looks though all lines
     let mut total_fuel: f32 = 0.0;
     for line in reader.lines() {
         let mass = line.expect("Failed to read line");
 
+        // makes sure we have some fuel to read (instead of just a blank line)
         if mass.is_empty() {
             continue;
         }
@@ -43,6 +48,7 @@ fn main() {
 
             let mut loop_mass: f32 = mass.parse::<f32>().unwrap();
             'fuel_loop: loop {
+                // set it to itself so we can keep looping
                 loop_mass = caluclate_fuel_for_mass(loop_mass);
                 if debug == "1" {
                     println!("Fuel: {loop_mass}");
