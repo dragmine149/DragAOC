22c13eebb467c6ac94884b6706014631110dcd59
dragmine <dan@thebanners.uk>
2024-12-11 15:49:17 +0000

    Comments and clippy

--- aoc-2024/src/day11.rs
+++ aoc-2024/src/day11.rs
@@ -1,13 +1,13 @@
-use std::usize;
-
 use aoc_runner_derive::{aoc, aoc_generator};
+#[allow(unused_imports)]
 use num_format::{Locale, ToFormattedString};
+#[warn(unused_imports)]
 use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
 
 #[derive(Debug, Clone, Copy, PartialEq, Ord, Eq, PartialOrd)]
 struct Stone {
-    number: usize,
-    count: usize,
+    number: usize, // the number of this stone
+    count: usize,  // how many of this stone exists
 }
 
 impl Stone {
@@ -19,11 +19,13 @@ impl Stone {
         self.number = 1;
     }
     fn split_stone(&mut self) -> Stone {
+        // using maths YAY get the upper and lower half of the numbers
         let number_size = self.get_number_length();
         let stone_a = self.number / 10_usize.pow(number_size / 2);
         let stone_b = self.number % 10_usize.pow(number_size / 2);
 
         self.number = stone_a;
+        // Make the same amount of stones just with the lower number
         Stone {
             number: stone_b,
             count: self.count,
@@ -33,6 +35,7 @@ impl Stone {
         self.number *= 2024;
     }
     fn process_rules(&mut self) -> Option<Stone> {
+        // Process all the possible rules for the stones in order.
         if self.number == 0 {
             self.increase_stone();
             return None;
@@ -44,6 +47,7 @@ impl Stone {
         None
     }
     fn is_stone(&self, other: Stone) -> bool {
+        // Checks if a stone is a stone, could have probably done this somehow using `==` but it works.
         self.number == other.number
     }
 }
@@ -60,8 +64,9 @@ fn parse(input: &str) -> Vec<Stone> {
 }
 
 fn blink(stone_list: &mut [Stone]) -> Vec<Option<Stone>> {
-    let mut new_stones: Vec<Option<Stone>> = vec![];
+    let mut new_stones: Vec<Option<Stone>> = vec![]; // list of new empty stones made from the stones
 
+    // Get all the stones to process the rules at once
     stone_list
         .par_iter_mut()
         .map(|stone| stone.process_rules())
@@ -70,6 +75,7 @@ fn blink(stone_list: &mut [Stone]) -> Vec<Option<Stone>> {
     new_stones
 }
 
+// Special count function as `.len` won't return the true value
 fn count_stones(stones: &[Stone]) -> usize {
     stones
         .iter()
@@ -78,6 +84,7 @@ fn count_stones(stones: &[Stone]) -> usize {
         .expect("Failed to reduce stone list")
 }
 
+// Allows the main stone list to be kept small. Always combines duplicates when possible
 fn collapse_stones(stones: &[Stone]) -> Vec<Stone> {
     let mut new_stones: Vec<Stone> = vec![];
 
@@ -97,10 +104,9 @@ fn collapse_stones(stones: &[Stone]) -> Vec<Stone> {
     new_stones
 }
 
-#[aoc(day11, part1)]
-fn part1(input: &[Stone]) -> usize {
+fn blink_x_times(input: &[Stone], times: usize) -> usize {
     let mut stones: Vec<Stone> = input.to_vec();
-    for _blink in 0..25 {
+    for _blink in 0..times {
         // println!(
         //     "Blink: {}. Count: {:#?}",
         //     _blink,
@@ -108,10 +114,8 @@ fn part1(input: &[Stone]) -> usize {
         // );
 
         let new_stones = blink(&mut stones);
-        for stone in new_stones.iter() {
-            if let Some(result) = stone {
-                stones.push(*result);
-            }
+        for stone in new_stones.iter().flatten() {
+            stones.push(*stone);
         }
         stones = collapse_stones(&stones);
     }
@@ -120,37 +124,30 @@ fn part1(input: &[Stone]) -> usize {
     count_stones(&stones)
 }
 
+#[aoc(day11, part1)]
+fn part1(input: &[Stone]) -> usize {
+    blink_x_times(input, 25)
+}
+
 #[aoc(day11, part2)]
 fn part2(input: &[Stone]) -> usize {
-    let mut stones: Vec<Stone> = input.to_vec();
-    for _blink in 0..75 {
-        println!(
-            "Blink: {}. Count: {:#?}",
-            _blink,
-            count_stones(&stones).to_formatted_string(&Locale::en_GB)
-        );
-
-        let new_stones = blink(&mut stones);
-        for stone in new_stones.iter() {
-            if let Some(result) = stone {
-                stones.push(*result);
-            }
-        }
-        stones = collapse_stones(&stones);
-    }
-
-    println!("Length: {}", stones.len());
-    count_stones(&stones)
+    blink_x_times(input, 75)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // only one test and example today
     const EXAMPLE_1: &str = "125 17";
 
     #[test]
     fn part1_example() {
         assert_eq!(part1(&parse(EXAMPLE_1)), 55312);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse(EXAMPLE_1)), 65601038650482);
     }
 }
