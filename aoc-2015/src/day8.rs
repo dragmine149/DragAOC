9b20316a6ca052e3ac4fd838f0e0c554428315fd
dragmine <dan@thebanners.uk>
2025-01-03 02:20:05 +0000

    Improved code

--- aoc-2015/src/day8.rs
+++ aoc-2015/src/day8.rs
@@ -1,12 +1,9 @@
 use aoc_runner_derive::aoc;
 
-#[aoc(day8, part1)]
-fn part1(input: &str) -> u64 {
+fn process(input: &mut dyn Iterator<Item = String>) -> u64 {
     let mut sum_a: u64 = 0;
     let mut sum_b: u64 = 0;
     input
-        .lines()
-        .skip_while(|line| line.is_empty())
         .map(|line| {
             let code_count = line.len();
             // println!("{}", line);
@@ -43,48 +40,24 @@ fn part1(input: &str) -> u64 {
     sum_a - sum_b
 }
 
+#[aoc(day8, part1)]
+fn part1(input: &str) -> u64 {
+    process(
+        &mut input
+            .lines()
+            .skip_while(|line| line.is_empty())
+            .map(|line| line.to_string()),
+    )
+}
+
 #[aoc(day8, part2)]
 fn part2(input: &str) -> u64 {
-    let mut sum_a: u64 = 0;
-    let mut sum_b: u64 = 0;
-    input
-        .lines()
-        .skip_while(|line| line.is_empty())
-        .map(|line| format!("{:?}", line))
-        .map(|line| {
-            let code_count = line.len();
-
-            let mut true_size = 0;
-            let mut iter = line.chars();
-            iter.next();
-            while let Some(c) = iter.next() {
-                if c == '\\' {
-                    let x = iter.next();
-                    if x.unwrap() == 'x' {
-                        // true_size += 3;
-                        // true_size += 1;
-                        iter.next();
-                        iter.next();
-                        // } else {
-                        // true_size += 1;
-                    }
-                }
-                true_size += 1;
-            }
-            true_size -= 1;
-
-            // println!("{}", line);
-            // println!("{:?}", code_count);
-            // println!("{:?}", true_size);
-
-            (code_count as u64, true_size)
-        })
-        .for_each(|line| {
-            sum_a += line.0;
-            sum_b += line.1;
-        });
-
-    sum_a - sum_b
+    process(
+        &mut input
+            .lines()
+            .skip_while(|line| line.is_empty())
+            .map(|line| format!("{:?}", line)),
+    )
 }
 
 #[cfg(test)]
