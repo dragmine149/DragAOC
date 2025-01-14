f2b3b96b00d1fa1225b3f9646c1184ec958a9a35
dragmine <dan@thebanners.uk>
2025-01-14 17:26:01 +0000

    Re-wrote a lot of day 6 to work with the utils file and be faster in general

--- aoc-2024/src/day06.rs
+++ aoc-2024/src/day06.rs
@@ -1,16 +1,90 @@
+use crate::utils::{Direction, Grid, Position};
 use aoc_runner_derive::{aoc, aoc_generator};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum Cell {
+    Empty,
+    Obstruction,
+    North,
+    East,
+    South,
+    West,
+    Visited,
+    Block,
+}
+
+impl std::fmt::Debug for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Empty => '.',
+                Self::Obstruction => '#',
+                Self::North => '^',
+                Self::East => '>',
+                Self::South => 'v',
+                Self::West => '<',
+                Self::Visited => 'X',
+                Self::Block => 'O',
+            }
+        )
+    }
+}
+
+impl From<Direction> for Cell {
+    fn from(value: Direction) -> Self {
+        match value {
+            Direction::North => Self::North,
+            Direction::East => Self::East,
+            Direction::South => Self::South,
+            Direction::West => Self::West,
+            Direction::NorthEast => panic!("Invalid direction"),
+            Direction::SouthEast => panic!("Invalid direction"),
+            Direction::SouthWest => panic!("Invalid direction"),
+            Direction::NorthWest => panic!("Invalid direction"),
+        }
+    }
+}
+
+impl From<Cell> for Direction {
+    fn from(value: Cell) -> Self {
+        match value {
+            Cell::Empty => panic!("Invalid Cell"),
+            Cell::Obstruction => panic!("Invalid Cell"),
+            Cell::North => Self::North,
+            Cell::East => Self::East,
+            Cell::South => Self::South,
+            Cell::West => Self::West,
+            Cell::Visited => panic!("Invalid Cell"),
+            Cell::Block => panic!("Invalid Cell"),
+        }
+    }
+}
 
 #[aoc_generator(day6)]
-fn parse(input: &str) -> (Vec<Vec<u8>>, (u8, u8)) {
-    let mut guard = (0_u8, 0_u8);
+fn parse(input: &str) -> (Grid<Cell>, Position) {
+    let grid = Grid::from_str(input, Cell::Empty, |cell| match cell {
+        '.' => Cell::Empty,
+        '#' => Cell::Obstruction,
+        '^' => Cell::North,
+        '>' => Cell::East,
+        'v' => Cell::South,
+        '<' => Cell::West,
+        'X' => Cell::Visited,
+        'O' => Cell::Block,
+        _ => panic!("Invalid cell!"),
+    });
 
     // get the location of the guard
+    let mut guard: Position = Position::empty();
     for (index, line) in input.lines().enumerate() {
         let guard_potential_pos = line.find('^');
 
         match guard_potential_pos {
             Some(pos) => {
-                guard = (index as u8, pos as u8);
+                guard = Position(pos, index);
             }
             None => {
                 continue;
@@ -18,36 +92,7 @@ fn parse(input: &str) -> (Vec<Vec<u8>>, (u8, u8)) {
         }
     }
 
-    // translate the map into a grid of numbers.
-    let map = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|char| match char {
-                    '#' => 1,
-                    '^' => 2,
-                    _ => 0,
-                })
-                .collect()
-        })
-        .collect();
-    (map, guard)
-}
-
-// Returns the next position to go to and if that is inside or outside of the map.
-fn translate_direction(pos: (u8, u8), direction: u8) -> (u8, u8, bool) {
-    if direction == 2 {
-        let out_of_map = pos.0 < 1;
-        return (pos.0.saturating_sub(1), pos.1, out_of_map);
-    }
-    if direction == 3 {
-        return (pos.0, pos.1.saturating_add(1), false);
-    }
-    if direction == 4 {
-        return (pos.0.saturating_add(1), pos.1, false);
-    }
-    let out_of_map = pos.1 < 1;
-    (pos.0, pos.1.saturating_sub(1), out_of_map)
+    (grid, guard)
 }
 
 // Move the guard around the map
@@ -58,74 +103,65 @@ fn translate_direction(pos: (u8, u8), direction: u8) -> (u8, u8, bool) {
 // - Is out of map: Is guard inside or outside of the border
 // - Position: Position of the guard (or last position guard was in before going outside)
 fn move_guard(
-    mut map: Vec<Vec<u8>>,
-    pos: (u8, u8),
-    direction: u8,
-) -> (Vec<Vec<u8>>, u16, bool, (u8, u8)) {
+    map: &mut Grid<Cell>,
+    mut pos: Position,
+    direction: Direction,
+) -> (u16, bool, Position) {
     let mut move_count: u16 = 0;
-    let mut direction_info = pos.to_owned();
     loop {
-        let next_dir_info = translate_direction(direction_info, direction);
-        if next_dir_info.0 >= map.len() as u8
-            || next_dir_info.1 >= map[0].len() as u8
-            || next_dir_info.2
-        {
-            return (map, move_count + 1, true, direction_info);
+        if !pos.is_next_valid(&direction, &map.get_size()) {
+            return (move_count + 1, true, pos);
         }
+        let next_dir_info = pos.next_pos(&direction);
+        let square = map.get_unmut_cell(&next_dir_info);
 
-        // println!(
-        //     "Currently checking cell: {:?}",
-        //     (next_dir_info.0, next_dir_info.1)
-        // );
-
-        let square = &map[next_dir_info.0 as usize][next_dir_info.1 as usize];
-
-        if square == &0 {
+        if square == Cell::Empty {
             // new square, mark it
             move_count += 1;
             // move guard by setting the next square to the guard and the previous square to an x.
             // This also accidently prevents an issue in p2
-            map[next_dir_info.0 as usize][next_dir_info.1 as usize] = direction;
-            map[direction_info.0 as usize][direction_info.1 as usize] = 6;
+            map.set_cell(&next_dir_info, Cell::from(direction));
+            map.set_cell(&pos, Cell::Visited);
 
-            direction_info = (next_dir_info.0, next_dir_info.1);
+            pos = next_dir_info;
             continue;
         }
-        if square == &1 || square == &7 {
+        if square == Cell::Obstruction || square == Cell::Block {
             // the square is an object
             break;
         }
-        if (&2..&5).contains(&square) {
+        if [Cell::North, Cell::East, Cell::South, Cell::West].contains(&square) {
             // another guard somehow
             panic!("Why is there another guard around?");
         }
-        if square == &6 {
+        if square == Cell::Visited {
             // the square has already been visited
 
-            map[next_dir_info.0 as usize][next_dir_info.1 as usize] = direction;
-            map[direction_info.0 as usize][direction_info.1 as usize] = 6;
+            map.set_cell(&next_dir_info, Cell::from(direction));
+            map.set_cell(&pos, Cell::Visited);
 
-            direction_info = (next_dir_info.0, next_dir_info.1);
+            pos = next_dir_info;
             continue;
         }
         panic!("Unknown square!");
     }
 
-    (map, move_count, false, direction_info)
+    (move_count, false, pos)
 }
 
 // Normal guard movement
-fn move_and_rotate_guard(input: &(Vec<Vec<u8>>, (u8, u8))) -> u16 {
+fn move_and_rotate_guard(input: &(Grid<Cell>, Position)) -> u16 {
     let mut is_out_map = false;
     let mut map = input.0.clone();
     let mut total_moves = 0;
-    let mut guard_direction = 2;
+    let mut guard_direction = Direction::North;
     let mut pos = input.1;
 
     while !is_out_map {
         // output_map(&map);
         let move_count;
-        (map, move_count, is_out_map, pos) = move_guard(map, pos, guard_direction);
+        (move_count, is_out_map, pos) = move_guard(&mut map, pos, guard_direction);
+        // println!("{:?}", map);
         // println!(
         //     "Guard moved {} and is {} in map",
         //     move_count,
@@ -134,33 +170,52 @@ fn move_and_rotate_guard(input: &(Vec<Vec<u8>>, (u8, u8))) -> u16 {
         total_moves += move_count;
 
         if !is_out_map {
-            // overflow rotation system
-            guard_direction += 1;
-            if guard_direction >= 6 {
-                guard_direction = 2;
-            }
+            guard_direction = guard_direction.rotate(false);
         }
-
-        // output_map(&map);
     }
-    // output_map(&map);
 
     total_moves
 }
 
-// Try to block the guard by doing the same as the above but slightly different.
-fn move_and_block_guard(input: &(Vec<Vec<u8>>, (u8, u8)), obstruction_pos: (u8, u8)) -> bool {
+fn move_and_path_guard(input: &(Grid<Cell>, Position)) -> Vec<Position> {
+    let mut is_out_map = false;
     let mut map = input.0.clone();
-    map[obstruction_pos.0 as usize][obstruction_pos.1 as usize] = 7; // the square to block
+    let mut guard_direction = Direction::North;
+    let mut pos = input.1;
+    let mut positions = vec![];
 
-    let mut guard_direction = 2;
+    while !is_out_map {
+        let end_pos;
+        (_, is_out_map, end_pos) = move_guard(&mut map, pos, guard_direction);
+
+        pos.iter_positions(&end_pos).into_iter().for_each(|p| {
+            if !positions.contains(&p) {
+                positions.push(p)
+            }
+        });
+        pos = end_pos;
+
+        if !is_out_map {
+            guard_direction = guard_direction.rotate(false);
+        }
+    }
+
+    positions
+}
+
+// Try to block the guard by doing the same as the above but slightly different.
+fn move_and_block_guard(input: &(Grid<Cell>, Position), obstruction_pos: Position) -> bool {
+    let mut map = input.0.clone();
+    map.set_cell(&obstruction_pos, Cell::Obstruction); // the square to block
+
+    let mut guard_direction = Direction::North;
     let mut pos = input.1;
     let mut is_out_map = false;
 
-    let mut visited: Vec<(u8, u8)> = vec![pos]; // list of positions
+    let mut visited: Vec<Position> = vec![pos]; // list of positions
 
     while !is_out_map {
-        (map, _, is_out_map, pos) = move_guard(map, pos, guard_direction);
+        (_, is_out_map, pos) = move_guard(&mut map, pos, guard_direction);
 
         // If the guard has been here before break. (Pos is only when the guard rotates, and since we're back here we are 99% likely to be in a loop)
         // The `visited.last().expect("Failed to find last elm") != &pos` check prevents the guard from giving up if it has just rotated and already got stuck without moving.
@@ -174,12 +229,7 @@ fn move_and_block_guard(input: &(Vec<Vec<u8>>, (u8, u8)), obstruction_pos: (u8, 
 
         // println!("{:?}\t{:?}\t{:?}", visited, pos, is_out_map);
         if !is_out_map {
-            // println!("{}", guard_direction);
-            guard_direction += 1;
-            if guard_direction >= 6 {
-                guard_direction = 2;
-            }
-            // println!("{}", guard_direction);
+            guard_direction = guard_direction.rotate(false);
         }
     }
     // output_map(&map);
@@ -187,54 +237,27 @@ fn move_and_block_guard(input: &(Vec<Vec<u8>>, (u8, u8)), obstruction_pos: (u8, 
     false
 }
 
-// Debug, convert and print map.
-#[allow(dead_code)]
-fn output_map(map: &[Vec<u8>]) {
-    let output_map = map
-        .iter()
-        .map(|line| {
-            let mut visible_line = line
-                .iter()
-                .map(|pos| match *pos {
-                    0 => '.',
-                    1 => '#',
-                    2 => '^',
-                    3 => '>',
-                    4 => 'v',
-                    5 => '<',
-                    6 => 'X',
-                    7 => 'O',
-                    _ => '?',
-                })
-                .collect::<String>();
-            visible_line.push('\n');
-            visible_line
-        })
-        .collect::<String>();
-    println!("{}", output_map);
-}
-
 #[aoc(day6, part1)]
-fn part1(input: &(Vec<Vec<u8>>, (u8, u8))) -> u16 {
+fn part1(input: &(Grid<Cell>, Position)) -> u16 {
     move_and_rotate_guard(input)
 }
 #[aoc(day6, part2)]
-fn part2(input: &(Vec<Vec<u8>>, (u8, u8))) -> u16 {
-    let mut obstruction_count = 0;
-    for (line_index, line) in input.0.iter().enumerate() {
-        for (pos_index, _) in line.iter().enumerate() {
-            // println!("Adding obstruction at {:?}", (line_index, pos_index));
-            if input.0[line_index][pos_index] == 1 {
-                // println!("Canceled! Obstruction already at place.");
-                continue;
+fn part2(input: &(Grid<Cell>, Position)) -> u16 {
+    move_and_path_guard(input)
+        .par_iter()
+        .map(|pos| {
+            if input.0.get_unmut_cell(&pos) == Cell::Obstruction {
+                return 0;
             }
 
-            let looped = move_and_block_guard(input, (line_index as u8, pos_index as u8));
-            obstruction_count += if looped { 1 } else { 0 }
-        }
-    }
-
-    obstruction_count
+            let looped = move_and_block_guard(input, *pos);
+            if looped {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
--- aoc-2024/src/utils.rs
+++ aoc-2024/src/utils.rs
@@ -1,112 +1,136 @@
 use core::fmt;
+use itertools::Itertools;
 use std::fmt::Write;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+/**
+Stores a position in a 2D array with the format of (X, Y)
+*/
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Position(pub usize, pub usize);
 
 impl From<usize> for Position {
+    /// a => (a, a)
     fn from(value: usize) -> Self {
         Self(value, value)
     }
 }
 impl From<&[usize]> for Position {
+    /// &[usize] => (a.0, a.1)
     fn from(value: &[usize]) -> Self {
         Self(value[0], value[1])
     }
 }
 impl From<&Vec<usize>> for Position {
+    /// vec![usize] => (a.0, a.1)
     fn from(value: &Vec<usize>) -> Self {
         Self(value[0], value[1])
     }
 }
 
+#[allow(dead_code)]
 impl Position {
-    #[allow(dead_code)]
+    /**
+    get the next position following that direction.
+    */
     pub fn next_pos(&self, direction: &Direction) -> Self {
         match direction {
-            Direction::North => Position(self.0 - 1, self.1),
-            Direction::East => Position(self.0, self.1 + 1),
-            Direction::South => Position(self.0 + 1, self.1),
-            Direction::West => Position(self.0, self.1 - 1),
+            Direction::West => Position(self.0 - 1, self.1),
+            Direction::South => Position(self.0, self.1 + 1),
+            Direction::East => Position(self.0 + 1, self.1),
+            Direction::North => Position(self.0, self.1 - 1),
+            Direction::SouthWest => Position(self.0 - 1, self.1 + 1),
+            Direction::SouthEast => Position(self.0 + 1, self.1 + 1),
+            Direction::NorthEast => Position(self.0 + 1, self.1 - 1),
+            Direction::NorthWest => Position(self.0 - 1, self.1 - 1),
         }
     }
 
-    #[allow(dead_code)]
+    /// Checks if the next position is valid
+    pub fn is_next_valid(&self, direction: &Direction, grid_size: &Position) -> bool {
+        match direction {
+            Direction::North => self.1 >= 1,
+            Direction::East => self.0 + 1 < grid_size.0,
+            Direction::South => self.1 + 1 < grid_size.1,
+            Direction::West => self.0 >= 1,
+            Direction::NorthEast => self.0 + 1 < grid_size.0 && self.1 >= 1,
+            Direction::SouthEast => self.0 + 1 < grid_size.0 && self.1 + 1 < grid_size.1,
+            Direction::SouthWest => self.0 >= 1 && self.1 + 1 < grid_size.1,
+            Direction::NorthWest => self.0 >= 1 && self.1 >= 1,
+        }
+    }
+
+    /// check if the current position is valid.
     pub fn is_valid(&self, grid_size: &Position) -> bool {
-        // pos can never be negative as that cause some rust errors.
+        // pos can never be negative as that would cause errors, hence no need to check for it.
         self.0 < grid_size.0 && self.1 < grid_size.1
     }
 
-    #[allow(dead_code)]
+    /// Create an empty one for other use.
     pub fn empty() -> Self {
         Self(0, 0)
     }
-    #[allow(dead_code)]
-    pub fn get_valid_positions(&self, grid_size: &Position) -> Vec<Position> {
-        let mut valid: Vec<Position> = vec![];
-        if self.0 >= 1 {
-            valid.push(Position(self.0 - 1, self.1));
-        }
-        if self.1 >= 1 {
-            valid.push(Position(self.0, self.1 - 1));
-        }
-        if self.0 < grid_size.1 {
-            valid.push(Position(self.0 + 1, self.1));
-        }
-        if self.1 < grid_size.0 {
-            valid.push(Position(self.0, self.1 + 1));
-        }
-        valid
+
+    pub const MAX: Self = Self(usize::MAX, usize::MAX);
+
+    /// get all our neighbours. (In all Directions)
+    pub fn get_neighbours(&self, grid_size: &Position) -> Vec<Position> {
+        self.get_valid_directions(grid_size)
+            .iter()
+            .map(|v| self.next_pos(v))
+            .collect_vec()
     }
 
-    #[allow(dead_code)]
+    /// get the valid directions we can move, if any.
     pub fn get_valid_directions(&self, grid_size: &Position) -> Vec<Direction> {
         let mut valid: Vec<Direction> = vec![];
         if self.0 >= 1 {
-            valid.push(Direction::North);
-        }
-        if self.1 >= 1 {
             valid.push(Direction::West);
         }
-        if self.0 < grid_size.0 {
-            valid.push(Direction::South);
+        if self.1 >= 1 {
+            valid.push(Direction::North);
         }
-        if self.1 < grid_size.1 {
+        if self.0 + 1 < grid_size.0 {
             valid.push(Direction::East);
         }
+        if self.1 + 1 < grid_size.1 {
+            valid.push(Direction::South);
+        }
+        if self.0 >= 1 && self.1 + 1 < grid_size.1 {
+            valid.push(Direction::SouthWest);
+        }
+        if self.0 + 1 < grid_size.0 && self.1 + 1 < grid_size.1 {
+            valid.push(Direction::SouthEast);
+        }
+        if self.0 + 1 < grid_size.0 && self.1 >= 1 {
+            valid.push(Direction::NorthEast);
+        }
+        if self.0 >= 1 && self.1 >= 1 {
+            valid.push(Direction::NorthWest);
+        }
         valid
     }
 
-    #[allow(dead_code)]
+    /// A combination of get_neighbours and get_valid_directions
     pub fn get_valid(&self, grid_size: &Position) -> Vec<(Direction, Position)> {
-        let mut valid: Vec<(Direction, Position)> = vec![];
-        if self.0 >= 1 {
-            valid.push((Direction::North, Position(self.0 - 1, self.1)));
-        }
-        if self.1 >= 1 {
-            valid.push((Direction::West, Position(self.0, self.1 - 1)));
-        }
-        if self.0 < grid_size.0 {
-            valid.push((Direction::South, Position(self.0 + 1, self.1)));
-        }
-        if self.1 < grid_size.1 {
-            valid.push((Direction::East, Position(self.0, self.1 + 1)));
-        }
-        valid
+        self.get_valid_directions(grid_size)
+            .iter()
+            .map(|v| (*v, self.next_pos(v)))
+            .collect_vec()
     }
 
-    #[allow(dead_code)]
+    /// return as a index number (used for 2d array in a 1d array situtation)
     pub fn get_as_number(&self, grid_size: &Position) -> usize {
         self.1 * grid_size.1 + self.0
     }
 
     // Thanks to guy_732: https://github.com/guy-732/aoc-2024/blob/01da016705b2be4cbbb199603d2cf32fcbcd0fda/src/day20.rs#L15-L34
-    #[allow(dead_code)]
+    /// get the point distance between two points
     pub fn manhattan_distance(&self, other: &Self) -> u64 {
         (self.0.abs_diff(other.0) + self.1.abs_diff(other.1)) as u64
     }
 
-    #[allow(dead_code)]
+    // Thanks to guy_732: https://github.com/guy-732/aoc-2024/blob/01da016705b2be4cbbb199603d2cf32fcbcd0fda/src/day20.rs#L15-L34
+    /// generate an iter of all positions within a radius of max_distance
     pub fn iter_positions_within(
         &self,
         max_distance: usize,
@@ -124,6 +148,21 @@ impl Position {
             .filter(move |(_, dist)| (*dist as usize) <= max_distance)
             .filter(move |(pos, _)| pos.0 < grid_size.0 && pos.1 < grid_size.1)
     }
+
+    /// loop over all positions between two positions
+    pub fn iter_positions<'a>(&'a self, end: &'a Self) -> impl IntoIterator<Item = Position> + '_ {
+        let (a, b) = if self < end { (self, end) } else { (end, self) };
+
+        ((a.0 as isize)..=(b.0 as isize)).flat_map(move |x_dist| {
+            ((a.1 as isize)..=(b.1 as isize))
+                .map(move |y_dist| Position(x_dist as usize, y_dist as usize))
+        })
+    }
+
+    /// check if itself is the corner of the grid
+    pub fn is_corner(&self, grid_size: &Position) -> bool {
+        (self.0 == grid_size.0 - 1 || self.0 == 0) && (self.1 == grid_size.1 - 1 || self.1 == 0)
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -132,23 +171,108 @@ pub enum Direction {
     East,
     South,
     West,
+    NorthEast,
+    SouthEast,
+    SouthWest,
+    NorthWest,
 }
 
+impl From<usize> for Direction {
+    fn from(value: usize) -> Self {
+        match value {
+            0 => Direction::North,
+            1 => Direction::East,
+            2 => Direction::South,
+            3 => Direction::West,
+            4 => Direction::NorthEast,
+            5 => Direction::SouthEast,
+            6 => Direction::SouthWest,
+            7 => Direction::NorthWest,
+            _ => panic!("Invalid direction"),
+        }
+    }
+}
+
+#[allow(dead_code)]
 impl Direction {
-    // Useful for quickly going in the opposite direction
-    #[allow(dead_code)]
+    /// Useful for quickly going in the opposite direction
     pub fn inverse(&self) -> Self {
         match self {
             Direction::East => Direction::West,
             Direction::West => Direction::East,
             Direction::North => Direction::South,
             Direction::South => Direction::North,
+            Direction::NorthEast => Direction::SouthWest,
+            Direction::SouthEast => Direction::NorthWest,
+            Direction::SouthWest => Direction::NorthEast,
+            Direction::NorthWest => Direction::SouthEast,
         }
     }
 
-    #[allow(dead_code)]
+    /// useful for debugging
     pub fn index(&self) -> usize {
         *self as usize
+    }
+
+    pub fn rotate(&self, diagonal: bool) -> Self {
+        match self {
+            Self::North => {
+                if diagonal {
+                    Self::NorthEast
+                } else {
+                    Self::East
+                }
+            }
+            Self::East => {
+                if diagonal {
+                    Self::SouthEast
+                } else {
+                    Self::South
+                }
+            }
+            Self::South => {
+                if diagonal {
+                    Self::SouthWest
+                } else {
+                    Self::West
+                }
+            }
+            Self::West => {
+                if diagonal {
+                    Self::NorthWest
+                } else {
+                    Self::North
+                }
+            }
+            Self::NorthEast => {
+                if diagonal {
+                    Self::East
+                } else {
+                    Self::SouthEast
+                }
+            }
+            Self::SouthEast => {
+                if diagonal {
+                    Self::South
+                } else {
+                    Self::SouthWest
+                }
+            }
+            Self::SouthWest => {
+                if diagonal {
+                    Self::West
+                } else {
+                    Self::NorthWest
+                }
+            }
+            Self::NorthWest => {
+                if diagonal {
+                    Self::North
+                } else {
+                    Self::NorthEast
+                }
+            }
+        }
     }
 }
 
@@ -162,6 +286,7 @@ impl<T: fmt::Debug> fmt::Debug for Grid<T> {
             self.iter()
                 .map(|y| {
                     let mut yy = y.iter().fold(String::new(), |mut output, x| {
+                        // allow each cell to debug how this wish to debug.
                         if f.alternate() {
                             let _ = write!(output, "{:#?}", x);
                             output
@@ -203,32 +328,99 @@ impl<T> std::ops::DerefMut for Grid<T> {
     }
 }
 
+#[allow(dead_code)]
 impl<T: std::clone::Clone> Grid<T> {
-    #[allow(dead_code)]
+    /// create a new grid of set size and fill with the default argument
     pub fn new(size: &Position, default_arg: T) -> Self {
         Self(vec![vec![default_arg; size.1]; size.0])
     }
 
-    #[allow(dead_code)]
+    /// create a grid from a string. Useful for loading from files.
+    ///
+    /// [translation] is a user-provided method to take the char and translate it into whatever the grid needs to be.
+    pub fn from_str<F: Fn(char) -> T>(s: &str, default_arg: T, translation: F) -> Self {
+        let mut grid = Self::new(
+            &Position::from(s.lines().nth(0).unwrap().len()),
+            default_arg,
+        );
+        s.lines().enumerate().for_each(|(y, line)| {
+            line.chars().enumerate().for_each(|(x, char)| {
+                grid.set_cell(&Position(x, y), translation(char));
+            });
+        });
+        grid
+    }
+
+    /// set a specific value to a specific cell.
+    ///
+    /// Prefered use of [get_cell] to set data if data has already been set
     pub fn set_cell(&mut self, pos: &Position, value: T) {
         self[pos.1][pos.0] = value;
     }
 
-    #[allow(dead_code)]
+    pub fn set_cells<'a>(&mut self, cells: impl IntoIterator<Item = &'a Position>, value: T) {
+        cells.into_iter().for_each(|&cell| {
+            self.set_cell(&cell, value.to_owned());
+        });
+    }
+
+    /// set all the corners of the grid.
+    pub fn set_corners(&mut self, value: T) {
+        self[0][0] = value.to_owned();
+        let size = self.get_size();
+        self[0][size.0 - 1] = value.to_owned();
+        self[size.1 - 1][0] = value.to_owned();
+        self[size.1 - 1][size.0 - 1] = value.to_owned();
+    }
+
+    /// set all the cells within a range of x to y.
+    pub fn set_cell_range(&mut self, start_pos: &Position, end_pos: &Position, value: T) {
+        for pos in start_pos.iter_positions(end_pos) {
+            self.set_cell(&pos, value.to_owned());
+        }
+    }
+
+    /// same as [set_cell_range] but calls the user provided [callback] function on the old cell value before setting it to the new cell value
+    pub fn get_set_cell_range<F: FnMut(&mut T) -> T>(
+        &mut self,
+        start_pos: &Position,
+        end_pos: &Position,
+        mut callback: F,
+    ) {
+        for pos in start_pos.iter_positions(end_pos) {
+            let cell = self.get_cell(&pos);
+            *cell = callback(cell);
+        }
+    }
+
+    /// returns a mutable copy of a cell
     pub fn get_cell(&mut self, pos: &Position) -> &mut T {
         &mut self[pos.1][pos.0]
     }
 
-    #[allow(dead_code)]
+    /// returns ownership of a cell value
     pub fn get_unmut_cell(&self, pos: &Position) -> T {
         self[pos.1][pos.0].to_owned()
     }
 
-    #[allow(dead_code)]
+    /// returns the size of the grid
     pub fn get_size(&self) -> Position {
         Position(
             self.first().expect("Failed to get first row").len(),
             self.len(),
         )
+    }
+
+    /// loop over all unmutable cell values
+    pub fn iter_unmut_cells(&self) -> impl IntoIterator<Item = T> + '_ {
+        let size = self.get_size();
+        (0..size.1)
+            .flat_map(move |y| (0..size.0).map(move |x| self.get_unmut_cell(&Position(x, y))))
+    }
+
+    /// loop over all positions in the grid
+    pub fn iter_positions(&self) -> impl IntoIterator<Item = Position> + '_ {
+        let size = self.get_size();
+        (0..size.1).flat_map(move |y| (0..size.0).map(move |x| Position(x, y)))
     }
 }
