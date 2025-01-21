e5a7d9eb6052d116396d340d58c8906009354332
dragmine <dan@thebanners.uk>
2025-01-21 23:59:32 +0000

    Collapsed, Commented, Clipped

--- aoc-2015/src/day21.rs
+++ aoc-2015/src/day21.rs
@@ -1,132 +1,51 @@
-use std::env::consts::ARCH;
-
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct Item {
-    cost: u16,
-    damage: u8,
-    armor: u8,
-}
-
-const WEAPONS: [Item; 5] = [
-    Item {
-        cost: 8,
-        damage: 4,
-        armor: 0,
-    },
-    Item {
-        cost: 10,
-        damage: 5,
-        armor: 0,
-    },
-    Item {
-        cost: 25,
-        damage: 6,
-        armor: 0,
-    },
-    Item {
-        cost: 40,
-        damage: 7,
-        armor: 0,
-    },
-    Item {
-        cost: 74,
-        damage: 8,
-        armor: 0,
-    },
+// From the AOC page, the shop selling stuff
+const WEAPONS: [(u8, u8, u8); 5] = [(8, 4, 0), (10, 5, 0), (25, 6, 0), (40, 7, 0), (74, 8, 0)];
+const ARMORS: [(u8, u8, u8); 6] = [
+    (0, 0, 0), // "Optional"
+    (13, 0, 1),
+    (31, 0, 2),
+    (53, 0, 3),
+    (75, 0, 4),
+    (102, 0, 5),
 ];
-const ARMORS: [Item; 6] = [
-    Item {
-        cost: 0,
-        damage: 0,
-        armor: 0,
-    },
-    Item {
-        cost: 13,
-        damage: 0,
-        armor: 1,
-    },
-    Item {
-        cost: 31,
-        damage: 0,
-        armor: 2,
-    },
-    Item {
-        cost: 53,
-        damage: 0,
-        armor: 3,
-    },
-    Item {
-        cost: 75,
-        damage: 0,
-        armor: 4,
-    },
-    Item {
-        cost: 102,
-        damage: 0,
-        armor: 5,
-    },
-];
-const RINGS: [Item; 7] = [
-    Item {
-        cost: 0,
-        damage: 0,
-        armor: 0,
-    },
-    Item {
-        cost: 25,
-        damage: 1,
-        armor: 0,
-    },
-    Item {
-        cost: 50,
-        damage: 2,
-        armor: 0,
-    },
-    Item {
-        cost: 100,
-        damage: 3,
-        armor: 0,
-    },
-    Item {
-        cost: 20,
-        damage: 0,
-        armor: 1,
-    },
-    Item {
-        cost: 40,
-        damage: 0,
-        armor: 2,
-    },
-    Item {
-        cost: 80,
-        damage: 0,
-        armor: 3,
-    },
+const RINGS: [(u8, u8, u8); 7] = [
+    (0, 0, 0), // "Optional"
+    (25, 1, 0),
+    (50, 2, 0),
+    (100, 3, 0),
+    (20, 0, 1),
+    (40, 0, 2),
+    (80, 0, 3),
 ];
 
-fn fight(status: &Item, boss_status: &Item) -> bool {
+// Fight the boss
+fn fight(status: &(u8, u8, u8), boss_status: &(u8, u8, u8)) -> bool {
     // println!("Fighting {:?} with {:?}", boss_status, status);
 
-    let damage_to_self = boss_status.damage.saturating_sub(status.armor).max(1);
-    let damage_to_boss = status.damage.saturating_sub(boss_status.armor).max(1);
+    // To save time and calculations, we don't actually need to simulate it
+    let damage_to_self = boss_status.1.saturating_sub(status.2).max(1);
+    let damage_to_boss = status.1.saturating_sub(boss_status.2).max(1);
 
-    let self_hits_to_death = (status.cost as u8).div_ceil(damage_to_self);
-    let boss_hits_to_death = (boss_status.cost as u8).div_ceil(damage_to_boss);
+    // Getting how many hits we can survive is more than enough as nothing changes
+    let self_hits_to_death = status.0.div_ceil(damage_to_self);
+    let boss_hits_to_death = boss_status.0.div_ceil(damage_to_boss);
 
     // println!(
     //     "Result: {:?} -> {:?}",
     //     self_hits_to_death, boss_hits_to_death
     // );
 
+    // Hence as long as we have more or equal (as we go first, it doesn't matter if they die in the same amount of hits)
+    // we win
     self_hits_to_death >= boss_hits_to_death
 }
 
 #[aoc_generator(day21)]
-fn parse(input: &str) -> Item {
-    let stats: (u8, u8, u8) = input
+fn parse(input: &str) -> (u8, u8, u8) {
+    input
         .lines()
         .map(|line| {
             line.split(": ")
@@ -136,15 +55,13 @@ fn parse(input: &str) -> Item {
                 .expect("Failed to turn into number")
         })
         .collect_tuple()
-        .expect("Failed to parse input");
-    Item {
-        cost: stats.0 as u16,
-        damage: stats.1,
-        armor: stats.2,
-    }
+        .expect("Failed to parse input")
 }
 
-fn shop<F: Fn(&Item, &Item, &Item, &Item) -> u16>(result: F) -> u16 {
+// Time to go shopping (the iter stack was too big)
+fn shop<F: Fn(&(u8, u8, u8), &(u8, u8, u8), &(u8, u8, u8), &(u8, u8, u8)) -> u16>(
+    result: F,
+) -> u16 {
     WEAPONS
         .iter()
         .map(|weapon| {
@@ -170,7 +87,10 @@ fn shop<F: Fn(&Item, &Item, &Item, &Item) -> u16>(result: F) -> u16 {
         .expect("Failed to get min of weapons")
 }
 
-fn shop_biased<F: Fn(&Item, &Item, &Item, &Item) -> u16>(result: F) -> u16 {
+// Shopping p2 but max this time
+fn shop_biased<F: Fn(&(u8, u8, u8), &(u8, u8, u8), &(u8, u8, u8), &(u8, u8, u8)) -> u16>(
+    result: F,
+) -> u16 {
     WEAPONS
         .iter()
         .map(|weapon| {
@@ -196,56 +116,48 @@ fn shop_biased<F: Fn(&Item, &Item, &Item, &Item) -> u16>(result: F) -> u16 {
         .expect("Failed to get min of weapons")
 }
 
-#[aoc(day21, part1)]
-fn part1(input: &Item) -> u16 {
-    shop(|weapon, armor, ring, ring2| {
-        if ring2 == ring {
-            return u16::MAX;
+fn process(
+    boss_status: &(u8, u8, u8),
+    weapon: &(u8, u8, u8),
+    armor: &(u8, u8, u8),
+    ring: &(u8, u8, u8),
+    ring2: &(u8, u8, u8),
+    failed: u16,
+) -> u16 {
+    // we can't have two of the sam item
+    if ring == ring2 {
+        // unless those items are neither
+        if ring.0 != 0 || ring.1 != 0 || ring.2 != 0 {
+            return failed;
         }
+    }
 
-        let self_status = Item {
-            cost: 100,
-            damage: weapon.damage + armor.damage + ring.damage + ring2.damage,
-            armor: weapon.armor + armor.armor + ring.armor + ring2.armor,
-        };
-        let result = fight(&self_status, input);
-        if result {
-            let cost = weapon.cost + armor.cost + ring.cost + ring2.cost;
-            // println!(
-            //     "Result of fighting with {:?}: {:?} ({:?}, {:?}, {:?}, {:?})",
-            //     self_status, cost, weapon, armor, ring, ring2
-            // );
-            cost
-        } else {
-            u16::MAX
-        }
-    })
+    // fight, let see who wins
+    let result = fight(
+        &(
+            100,
+            weapon.1 + armor.1 + ring.1 + ring2.1,
+            weapon.2 + armor.2 + ring.2 + ring2.2,
+        ),
+        boss_status,
+    );
+    // calculate and return.
+    if result {
+        weapon.0 as u16 + armor.0 as u16 + ring.0 as u16 + ring2.0 as u16
+    } else {
+        // it's free if we loose
+        failed
+    }
+}
+
+#[aoc(day21, part1)]
+fn part1(input: &(u8, u8, u8)) -> u16 {
+    shop(|weapon, armor, ring, ring2| process(input, weapon, armor, ring, ring2, u16::MAX))
 }
 
 #[aoc(day21, part2)]
-fn part2(input: &Item) -> u16 {
-    shop_biased(|weapon, armor, ring, ring2| {
-        if ring2 == ring {
-            return 0;
-        }
-
-        let self_status = Item {
-            cost: 100,
-            damage: weapon.damage + armor.damage + ring.damage + ring2.damage,
-            armor: weapon.armor + armor.armor + ring.armor + ring2.armor,
-        };
-        let result = fight(&self_status, input);
-        if result {
-            0
-        } else {
-            let cost = weapon.cost + armor.cost + ring.cost + ring2.cost;
-            // println!(
-            //     "Result of fighting with {:?}: {:?} ({:?}, {:?}, {:?}, {:?})",
-            //     self_status, cost, weapon, armor, ring, ring2
-            // );
-            cost
-        }
-    })
+fn part2(input: &(u8, u8, u8)) -> u16 {
+    shop_biased(|weapon, armor, ring, ring2| process(input, weapon, armor, ring, ring2, 0))
 }
 
 #[cfg(test)]
@@ -254,20 +166,6 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(
-            fight(
-                &Item {
-                    cost: 8,
-                    damage: 5,
-                    armor: 5
-                },
-                &Item {
-                    cost: 12,
-                    damage: 7,
-                    armor: 2,
-                }
-            ),
-            true
-        );
+        assert_eq!(fight(&(8, 5, 5), &(12, 7, 2)), true);
     }
 }
