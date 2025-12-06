e1b5078f8b9ce6907caad1ed1ce33ddf0e5703be
Dragmine149 <dan@thebanners.uk>
2025-12-06 11:12:37 +0000

    Day 6 Part 2 Completed

--- aoc-2025/src/day06.rs
+++ aoc-2025/src/day06.rs
@@ -1,4 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
 #[derive(Debug)]
 pub enum Operation {
@@ -19,6 +20,7 @@ impl From<char> for Operation {
 #[derive(Debug)]
 pub struct Calculation {
     numbers: Vec<usize>,
+    raw_numbers: Vec<String>,
     operation: Operation,
     size: usize,
 }
@@ -32,8 +34,8 @@ impl Calculation {
     }
 }
 
-#[aoc_generator(day6)]
-fn parse(input: &str) -> Vec<Calculation> {
+#[aoc_generator(day6, part1)]
+fn parse_p1(input: &str) -> Vec<Calculation> {
     let mut calculations = vec![];
     let mut size_count = 0_usize;
     let mut previous_char: char = char::default();
@@ -43,6 +45,7 @@ fn parse(input: &str) -> Vec<Calculation> {
             if previous_char != char::default() {
                 calculations.push(Calculation {
                     numbers: vec![],
+                    raw_numbers: vec![],
                     operation: Operation::from(previous_char),
                     size: size_count - 1,
                 });
@@ -58,6 +61,7 @@ fn parse(input: &str) -> Vec<Calculation> {
     });
     calculations.push(Calculation {
         numbers: vec![],
+        raw_numbers: vec![],
         operation: Operation::from(previous_char),
         size: size_count,
     });
@@ -93,16 +97,103 @@ fn parse(input: &str) -> Vec<Calculation> {
     calculations
 }
 
+#[aoc_generator(day6, part2)]
+fn parse_p2(input: &str) -> Vec<Calculation> {
+    let mut calculations = vec![];
+    let mut size_count = 0_usize;
+    let mut previous_char: char = char::default();
+    // For now, we are just going to ignore the weird spacing. However i fell like its going to come back to bite me.
+    input.lines().last().unwrap().chars().for_each(|char| {
+        if char.is_ascii_punctuation() {
+            if previous_char != char::default() {
+                calculations.push(Calculation {
+                    numbers: vec![],
+                    raw_numbers: vec![],
+                    operation: Operation::from(previous_char),
+                    size: size_count - 1,
+                });
+            }
+            size_count = 1;
+            previous_char = char;
+            return;
+        }
+        if char.is_whitespace() {
+            size_count += 1;
+            return;
+        }
+    });
+    calculations.push(Calculation {
+        numbers: vec![],
+        raw_numbers: vec![],
+        operation: Operation::from(previous_char),
+        size: size_count,
+    });
+
+    input
+        .lines()
+        .enumerate()
+        .take_while(|(count, _)| *count != input.lines().count() - 1)
+        .for_each(|(_, line)| {
+            // println!("{:?}", line);
+            let mut pos = 0;
+            calculations.iter_mut().for_each(|calc| {
+                let size = if calc.size == 1 {
+                    line.len() - pos
+                } else {
+                    calc.size
+                };
+                calc.raw_numbers.push(line[pos..pos + size].to_string());
+                pos += size + 1;
+            });
+        });
+
+    calculations.iter_mut().for_each(|calc| {
+        let length = calc
+            .raw_numbers
+            .iter()
+            .map(|num| num.len())
+            .max()
+            .expect("No max length?");
+        calc.raw_numbers.iter_mut().for_each(|num| {
+            while num.len() != length {
+                num.push(' ');
+            }
+        });
+        for pos in (0..length).rev() {
+            // print!(
+            //     "{:?}",
+            //     calc.raw_numbers
+            //         .iter()
+            //         .map(|num| num.get(pos..pos + 1).expect("Should be pos").to_string())
+            //         .collect::<Vec<String>>()
+            // );
+            let nums = calc
+                .raw_numbers
+                .iter()
+                .map(|num| num.get(pos..pos + 1).expect("Should be pos").to_string())
+                .join("");
+            calc.numbers.push(
+                nums.replace(" ", "")
+                    .parse::<usize>()
+                    .expect(&format!("Should be a num ({:?})", nums)),
+            );
+        }
+    });
+    // println!("{:?}", calculations);
+
+    calculations
+}
+
 #[aoc(day6, part1)]
 fn part1(input: &[Calculation]) -> usize {
     // println!("{:#?}", input);
     input.iter().map(|c| c.calculate_p1()).sum()
 }
 
-// #[aoc(day6, part2)]
-// fn part2(input: &str) -> String {
-//     todo!()
-// }
+#[aoc(day6, part2)]
+fn part2(input: &[Calculation]) -> usize {
+    input.iter().map(|c| c.calculate_p1()).sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -110,10 +201,10 @@ mod tests {
 
     #[test]
     fn part_1_example_1() {
-        let parsed = parse(
+        let parsed = parse_p1(
             "123
-45
-6
+ 45
+  6
 *",
         );
 
@@ -123,7 +214,7 @@ mod tests {
 
     #[test]
     fn part_1_example_2() {
-        let parsed = parse(
+        let parsed = parse_p1(
             "328
 64
 98
@@ -135,7 +226,7 @@ mod tests {
 
     #[test]
     fn part_1_example_3() {
-        let parsed = parse(
+        let parsed = parse_p1(
             " 51
 387
 215
@@ -147,7 +238,7 @@ mod tests {
 
     #[test]
     fn part_1_example_4() {
-        let parsed = parse(
+        let parsed = parse_p1(
             "64
 23
 314
@@ -160,7 +251,7 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(
-            part1(&parse(
+            part1(&parse_p1(
                 "123 328  51 64
  45 64  387 23
   6 98  215 314
@@ -171,8 +262,66 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn part2_example() {
-    //     assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
-    // }
+    #[test]
+    fn part_2_example_1() {
+        let parsed = parse_p2(
+            "123
+ 45
+  6
+*",
+        );
+
+        assert_eq!(parsed.get(0).unwrap().numbers, vec![356, 24, 1]);
+        assert_eq!(part1(&parsed), 8544);
+    }
+
+    #[test]
+    fn part_2_example_2() {
+        let parsed = parse_p2(
+            "328
+64
+98
++",
+        );
+        assert_eq!(parsed.get(0).unwrap().numbers, vec![8, 248, 369]);
+        assert_eq!(part1(&parsed), 625);
+    }
+
+    #[test]
+    fn part_2_example_3() {
+        let parsed = parse_p2(
+            " 51
+387
+215
+*",
+        );
+        assert_eq!(parsed.get(0).unwrap().numbers, vec![175, 581, 32]);
+        assert_eq!(part1(&parsed), 3253600);
+    }
+
+    #[test]
+    fn part_2_example_4() {
+        let parsed = parse_p2(
+            "64
+23
+314
++",
+        );
+        assert_eq!(parsed.get(0).unwrap().numbers, vec![4, 431, 623]);
+        assert_eq!(part1(&parsed), 1058);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(
+            part1(&parse_p2(
+                "123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +
+"
+            )),
+            3263827
+        );
+    }
 }
