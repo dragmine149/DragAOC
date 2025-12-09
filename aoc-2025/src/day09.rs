935fd124f24b2101f2d51a7be32114608e55d291
Dragmine149 <dan@thebanners.uk>
2025-12-09 18:18:52 +0000

    Day 9 Part 2 Checkpoint 2

--- aoc-2025/src/day09.rs
+++ aoc-2025/src/day09.rs
@@ -58,6 +58,23 @@ impl From<&str> for Positions {
     }
 }
 
+pub enum InsideType {
+    Outside,
+    Inside,
+    Specified,
+    Line,
+}
+impl InsideType {
+    fn next(&self) -> InsideType {
+        match self {
+            InsideType::Outside => InsideType::Inside,
+            InsideType::Inside => InsideType::Outside,
+            InsideType::Specified => todo!(),
+            InsideType::Line => todo!(),
+        }
+    }
+}
+
 ///
 /// XXXXX...XXXXX
 /// ....X...X...X
@@ -65,66 +82,61 @@ impl From<&str> for Positions {
 /// ....XXXXX....
 ///
 
-fn is_outside_perimeter(
+fn is_inside_perimeter(
     perimeter: &[Positions],
     position: &Positions,
-    cache: &mut HashMap<Positions, usize>,
+    cache: &mut HashMap<Positions, bool>,
 ) -> bool {
-    // println!();
-    // println!("Looking at {:?}", position);
     if let Some(cached) = cache.get(position) {
         // println!("Returning from cache!");
         // println!();
-
-        return cached.is_multiple_of(2);
+        return *cached;
     }
+    println!();
+    println!("Looking at {:?}", position);
 
     let start = &&perimeter[0].clone();
     let mut iter = perimeter.iter().peekable();
-    let mut count = 0_usize;
+    let mut inside = false;
     while let Some(line_start) = iter.next() {
         let line_end = iter.peek().unwrap_or(&&start);
 
         // if it's either linestart or lineend it's on the line, hence must be inside.
         if position == line_start || position == *line_end {
-            cache.insert(position.to_owned(), 101);
-            // println!("Point is on line, early break");
-            return false;
+            // we start here, so ofc it's true
+            inside = true;
         }
 
         if position.multi_inbetween_col(line_start, line_end)
             && position.multi_inbetween_row(line_start, line_end)
         {
-            // println!("In grid line");
-            // count += 1;
-            // continue;
-            cache.insert(position.to_owned(), 51);
-            // println!("In grid line");
-            return false;
+            // well, we are in between 2 lines...
+            // i guess, its true...
+            inside = true;
         }
 
-        if position.same_row(line_start) || position.same_row(line_end) {
-            continue;
-        }
+        // if position.same_row(line_start) || position.same_row(line_end) {
+        //     continue;
+        // }
 
-        // println!("{:?}, {:?}", line_start, line_end);
+        println!("{:?}, {:?}", line_start, line_end);
 
         if line_start.same_col(line_end) {
             if position.multi_inbetween_row(line_start, line_end) {
                 // println!("+=1");
-                count += 1;
+                inside = !inside;
             }
         }
     }
 
-    // println!("{:?} is {:?}", position, count);
-    // println!();
-    cache.insert(position.to_owned(), count);
+    println!("{:?} is {:?}", position, inside);
+    println!();
+    cache.insert(position.to_owned(), inside);
 
     // if odd, then we passed through 1 wall not 2.
     // count != 0 as passing though zero walls means we are outside.
     // !count.is_multiple_of(2) && count != 0
-    count.is_multiple_of(2)
+    inside
 }
 
 #[aoc_generator(day9)]
@@ -149,7 +161,7 @@ fn part1(input: &[Positions]) -> isize {
 
 #[aoc(day9, part2)]
 fn part2(input: &[Positions]) -> isize {
-    let mut cache = HashMap::<Positions, usize>::new();
+    let mut cache = HashMap::<Positions, bool>::new();
     let res = input
         .iter()
         .map(|pos| {
@@ -158,8 +170,10 @@ fn part2(input: &[Positions]) -> isize {
                 .filter(|pos2| pos.0 != pos2.0 && pos.1 != pos2.1)
                 .map(|pos2| pos.get_questionable_corners(pos2))
                 .filter(|corners| {
-                    !is_outside_perimeter(input, &corners[0], &mut cache)
-                        && !is_outside_perimeter(input, &corners[1], &mut cache)
+                    let inside = is_inside_perimeter(input, &corners[0], &mut cache)
+                        && is_inside_perimeter(input, &corners[1], &mut cache);
+                    println!("{:?} inside: {:?}", corners, inside);
+                    inside
                 })
                 // .inspect(|c| println!("{:?}", c))
                 .map(|corners| corners[0].area(&corners[1]))
@@ -171,7 +185,7 @@ fn part2(input: &[Positions]) -> isize {
         .max()
         // .inspect(|c| println!("{:?}", c))
         .unwrap();
-    // println!("{:?}", cache);
+    println!("{:?}", cache);
     res
 }
 
@@ -189,6 +203,67 @@ mod tests {
 7,3
 ";
 
+    const CUSTOM_EXAMPLE_1: &str = "11,1
+11,6
+9,6
+9,5
+5,5
+5,7
+2,7
+2,3
+7,3
+7,1
+";
+
+    const COMMUNITY_1: &str = "1,0
+3,0
+3,6
+16,6
+16,0
+18,0
+18,9
+13,9
+13,7
+6,7
+6,9
+1,9";
+
+    const COMMUNITY_2: &str = "1,1
+8,1
+8,3
+3,3
+3,4
+8,4
+8,9
+18,9
+18,11
+5,11
+5,9
+4,9
+4,11
+1,11
+1,7
+6,7
+6,6
+1,6";
+
+    const COMMUNITY_3: &str = "1,5
+3,5
+3,8
+7,8
+7,5
+9,5
+9,10
+11,10
+11,3
+6,3
+6,7
+4,7
+4,1
+13,1
+13,12
+1,12";
+
     #[test]
     fn part1_example() {
         assert_eq!(part1(&parse(EXAMPLE_1)), 50);
@@ -197,6 +272,26 @@ mod tests {
     #[test]
     fn part2_example() {
         assert_eq!(part2(&parse(EXAMPLE_1)), 24);
+    }
+
+    #[test]
+    fn part2_custom_example_1() {
+        assert_eq!(part2(&parse(CUSTOM_EXAMPLE_1)), 24);
+    }
+
+    #[test]
+    fn part2_community_1() {
+        assert_eq!(part2(&parse(COMMUNITY_1)), 30);
+    }
+
+    #[test]
+    fn part2_community_2() {
+        assert_eq!(part2(&parse(COMMUNITY_2)), 88);
+    }
+
+    #[test]
+    fn part2_community_3() {
+        assert_eq!(part2(&parse(COMMUNITY_3)), 72);
     }
 
     #[test]
