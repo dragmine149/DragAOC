e5322eec82e9514c840f604a0c09f93e7af055eb
Dragmine149 <dan@thebanners.uk>
2025-12-12 22:38:37 +0000

    Day 12 Part 1 Checkpoint 1

--- aoc-2025/src/day12.rs
+++ aoc-2025/src/day12.rs
@@ -1,46 +1,142 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
-#[derive(Debug, Hash)]
-pub struct Present {
-    pub id: u8,
-    pub sq1: bool,
-    pub sq2: bool,
-    pub sq3: bool,
-    pub sq4: bool,
-    pub sq5: bool,
-    pub sq6: bool,
-    pub sq7: bool,
-    pub sq8: bool,
-    pub sq9: bool,
+#[derive(Debug)]
+pub struct Present([bool; 9]);
+
+impl Present {
+    fn area_sum(&self) -> u8 {
+        self.0.iter().filter(|b| **b).count() as u8
+    }
 }
 
 #[aoc_generator(day12)]
-fn parse(input: &str) -> String {
-    // let iter = input.lines();
-    todo!()
+fn parse(input: &str) -> (Vec<Present>, Vec<(u8, u8, Vec<u8>)>) {
+    let mut iter = input.lines();
+    let mut presents = vec![];
+    for _ in 0..6 {
+        iter.next();
+        let present = iter
+            .by_ref()
+            .take(3)
+            .map(|s| {
+                s.chars().map(|c| match c {
+                    '#' => true,
+                    '.' => false,
+                    _ => panic!("Invalid character {:?}", c),
+                })
+            })
+            .flatten()
+            .collect_array()
+            .expect("An array of 9 items!");
+        presents.push(Present(present));
+        iter.next();
+    }
+    let grid = iter
+        .map(|line| {
+            let mut split = line.split(':');
+            let mut split2 = split.next().unwrap().split('x');
+            let width = split2.next().unwrap().parse::<u8>().unwrap();
+            let height = split2.next().unwrap().parse::<u8>().unwrap();
+            let count = split
+                .next()
+                .unwrap()
+                .split(' ')
+                .filter(|s| !s.is_empty())
+                .map(|s| {
+                    s.parse::<u8>()
+                        .unwrap_or_else(|_| panic!("Failed to parse {:?} to u8", s))
+                })
+                .collect_vec();
+            (width, height, count)
+        })
+        .collect_vec();
+    (presents, grid)
 }
 
 #[aoc(day12, part1)]
-fn part1(input: &str) -> String {
-    todo!()
+fn part1(input: &(Vec<Present>, Vec<(u8, u8, Vec<u8>)>)) -> usize {
+    // println!("{:?}", input);
+    input
+        .1
+        .iter()
+        .map(|i| {
+            let area = i.0 as u16 * i.1 as u16;
+            let min_required =
+                i.2.iter()
+                    .enumerate()
+                    .map(|(idx, want)| input.0[idx].area_sum() as u16 * *want as u16)
+                    .sum::<u16>();
+            if min_required > area {
+                return 0;
+            }
+            if min_required as f64 * 1.5 > area as f64 {
+                return 2;
+            }
+
+            println!(
+                "{:?} (min: {:?}, area: {:?})",
+                i,
+                min_required as f64 * 1.5,
+                area
+            );
+            1
+        })
+        .filter(|i| *i == 1)
+        .count()
 }
 
-#[aoc(day12, part2)]
-fn part2(input: &str) -> String {
-    todo!()
-}
+// #[aoc(day12, part2)]
+// fn part2(input: &str) -> String {
+//     todo!()
+// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn part1_example() {
-        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
-    }
+    const EXAMPLE_1: &str = "0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
+";
 
     #[test]
-    fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    fn part1_example() {
+        assert_eq!(part1(&parse(EXAMPLE_1)), 2);
     }
+
+    // #[test]
+    // fn part2_example() {
+    //     assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    // }
 }
